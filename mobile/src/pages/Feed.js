f7159db16a22a42d9cff5cb6032cb08fc203892a
MatheusGonçalves <matheusglmonteiro@gmail.com>
2019-06-17 02:51:32 -0300

    adicionando projeto completo da semana omnistack

---
+++ mobile/src/pages/Feed.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Text, View, Image, TouchableOpacity } from 'react-native'
+
+import camera from '../assets/camera.png'
+
+export default class Feed extends Component {
+
+    static navigationOptions = ({ navigation }) => ({
+        headerRight: (
+             <TouchableOpacity style={{marginRight: 20}} onPress={() => navigation.navigate('New')}>
+                 <Image source={camera} />
+             </TouchableOpacity>
+        ), 
+    });
+
+    render() {
+        return (
+            <View>
+                <Text>  </Text>
+            </View>
+        )
+    }
+}
